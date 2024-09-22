35920fb6eb0203d17b939ec9061b2d8cba6b4794
Iván Moya <ivn.moya@proton.me>
2024-09-22 22:41:39 +0200

    toc

--- Clase Introducción/graficas.m
+++ Clase Introducción/graficas.m
@@ -7,9 +7,9 @@ plot(0,0,"om")
 hold on
 hb1 = plot([0 A], [0 B], 'g');
 hb2 = plot([A A+B], [0 0 ], 'c');
-hpr = plot([A], [B], 'om');
+hpr = plot(A, B, 'om');
 hd = plot(A+B, 0, '.m');
-hpc = plot([A+B], [0], 'om');
+hpc = plot(A+B, 0, 'om');
 
 
 axis([-2 2 -2 2])
@@ -24,10 +24,10 @@ for t = 0:0.001:pi
 
     set(hb1, 'XData', [0 rx], 'YData', [0 ry])    
     set(hb2, 'XData', [rx cx(n)], 'YData', [ry cy(n)])
-    set(hpr, 'XData', [rx], 'YData',[ry])
+    set(hpr, 'XData', rx, 'YData',ry)
     set(hpc, 'XData',[cx(n)], 'YData',[cy(n)])
 
-    set(hd, 'XData',[cx], 'YData',cy)
+    set(hd, 'XData',cx, 'YData',cy)
 
     n = n+1;
     drawnow
